886209f34a9b832f5e1f597acce8dbf740866551
Parves <95443838+heyparves@users.noreply.github.com>
2023-12-26 12:01:04 +0600

    Update Hero.jsx

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
-        <div className="flex justify-center items-center flex-col h-[800px] relative z-10">
+        <div className="flex justify-center items-center flex-col h-[800px] relative z-100">
           <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
             Build & Grow
           </motion.h1>
