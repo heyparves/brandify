78c59cdb1b23285a727722e700c2bf12d64c6cbd
heyparves <heyparveshere@gmail.com>
2023-12-26 12:26:53 +0600

    Minor changes

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -8,16 +8,16 @@ import Button from "../button/Button";
 const Hero = () => {
   return (
     <section
-      className={`bg-[#D6DCE1] w-[100%] h-[100%] object-scale-[10p] bg-righ-top sm:pl-16 pl-6 overflow-hidden`}
+      className={`bg-[#D6DCE1] w-[100%] h-[100%] object-scale-[10p] bg-righ-top sm:pl-16 pl-6 overflow-hidden z-[1]`}
     >
       <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-        className={`${styles.innerWidth} mx-auto flex flex-col`}
+        className={`${styles.innerWidth} mx-auto flex flex-col z-10`}
       >
-        <div className="flex justify-center items-center flex-col h-[800px] relative z-100">
+        <div className="flex justify-center items-center flex-col h-[800px] relative z-10">
           <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
             Build & Grow
           </motion.h1>
