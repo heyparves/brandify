f3a2672a1e16967b15624dce314067a4fba3a49a
heyparves <heyparveshere@gmail.com>
2023-12-26 12:19:37 +0600

    Minor Update

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,19 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
+
+export const metadata = {
+  title: "Build & Grow Branding  - BigBrandify",
+  description:
+    "From Branding on online to sale, We provide best solutions to success and grow online",
+};
 
 const RootLayout = ({ children }) => (
   <html lang="en">
     <head>
       <link rel="preconnect" href="https://stijndv.com" />
-      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      <link
+        rel="stylesheet"
+        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+      />
     </head>
     <body>{children}</body>
   </html>
